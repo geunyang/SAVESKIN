b334e2f9040cc0ddb96ac9ed1c3dc27cb59b13c9
geunyang <gaeballza@gmail.com>
2022-05-31 17:55:05 +0900

    API

---
+++ saveskin/src/main/java/com/callor/app/model/RecallReturn.java
@@ -0,0 +1,15 @@
+package com.callor.app.model;
+
+import java.util.List;
+
+public class RecallReturn {
+
+	public String allCnt;// Integer 해당 메뉴의 전체 정보 개수
+	public String code;// Integer 결과코드
+	public String codeMsg;//
+	public List<RecallVO> content;
+}
+
+//class RecallChanell {
+//	RecallReturn return;
+//}
